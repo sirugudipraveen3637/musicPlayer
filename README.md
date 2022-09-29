4f51b04c4c6413ec19c6e8d5b6d92393e26d0c18
sirugudi praveen <sirugudipraveen@gmail.com>
2022-09-29 08:03:53 +0530

    initial

---
+++ README.md
@@ -0,0 +1,29 @@
+# MusicPlayer Assignment
+
+##### Published Url:-N/A
+
+
+<img src="https://github.com/sirugudipraveen3637/musicPlayer/blob/main/music.png" height="50%" width="100%"/>
+
+
+### Knowledge Gained:-
+
+  - Audio Tag
+  - Added play & pause methods
+  - Added volumeup and vlumedown
+  - Added previous and next methods
+
+  
+### Development Efforts:- 2.5hrs
+  
+### Technologies/Framework/Tool:-
+<span>
+
+<img src="https://img.shields.io/badge/-Java%20Script-yellowgreen"/>
+<img src="https://img.shields.io/badge/-HTML-blue"/>
+</span>
+
+
+### Author:- <b>Praveen Sirugudi<b>
+
+
---
+++ js/script2.js
@@ -0,0 +1,99 @@
+const wrapper = document.querySelector(".wrapper"),
+musicImg = wrapper.querySelector(".img-area img"),
+musicName = wrapper.querySelector(".song-details .name"),
+musicArtist = wrapper.querySelector(".song-details .artist"),
+playPauseBtn = wrapper.querySelector(".play-pause"),
+prevBtn = wrapper.querySelector("#prev"),
+volDownBtn = wrapper.querySelector("#down"),
+volUpBtn = wrapper.querySelector("#up"),
+mainAudio = wrapper.querySelector("#main-audio"),
+nextBtn = wrapper.querySelector("#next");
+let audioVolume=1.0;
+
+var musicIndex = Math.floor((Math.random() * allMusic.length) + 1);
+isMusicPaused = true;
+
+window.addEventListener("load", ()=>{
+  loadMusic(musicIndex);
+  //playMusic(); 
+});
+
+function loadMusic(indexNumb){
+  musicName.innerText = allMusic[indexNumb - 1].name;
+  musicArtist.innerText = allMusic[indexNumb - 1].artist;
+  musicImg.src = `images/${allMusic[indexNumb - 1].src}.jpg`;
+  mainAudio.src = `songs/${allMusic[indexNumb - 1].src}.mp3`;
+}
+
+//play music function
+function playMusic(){
+  wrapper.classList.add("paused");
+  playPauseBtn.querySelector("i").innerText = "pause";
+  mainAudio.play();
+}
+
+//pause music function
+function pauseMusic(){
+  wrapper.classList.remove("paused");
+  playPauseBtn.querySelector("i").innerText = "play_arrow";
+  mainAudio.pause();
+}
+
+//pause music function
+function decreaseVolume(){
+    
+    if(audioVolume>0.0)
+    audioVolume=audioVolume-0.20;
+    mainAudio.volume=audioVolume;
+  }
+
+  //pause music function
+function increaseVolume(){
+    audioVolume=audioVolume+0.20;
+    mainAudio.volume=audioVolume;
+  }
+  
+//prev music function
+function prevMusic(){
+  musicIndex--; //decrement of musicIndex by 1
+  //if musicIndex is less than 1 then musicIndex will be the array length so the last music play
+  musicIndex < 1 ? musicIndex = allMusic.length : musicIndex = musicIndex;
+  loadMusic(musicIndex);
+  playMusic();
+}
+
+//next music function
+function nextMusic(){
+  musicIndex++; //increment of musicIndex by 1
+  //if musicIndex is greater than array length then musicIndex will be 1 so the first music play
+  musicIndex > allMusic.length ? musicIndex = 1 : musicIndex = musicIndex;
+  loadMusic(musicIndex);
+  playMusic();
+}
+
+// play or pause button event
+playPauseBtn.addEventListener("click", ()=>{
+  const isMusicPlay = wrapper.classList.contains("paused");
+  //if isPlayMusic is true then call pauseMusic else call playMusic
+  isMusicPlay ? pauseMusic() : playMusic();
+});
+
+//prev music button event
+prevBtn.addEventListener("click", ()=>{
+  prevMusic();
+});
+
+//next music button event
+nextBtn.addEventListener("click", ()=>{
+  nextMusic();
+});
+
+//Decrease volume button event
+volDownBtn.addEventListener("click", ()=>{
+    decreaseVolume();
+  });
+
+  //Increase volume button event
+volUpBtn.addEventListener("click", ()=>{
+    increaseVolume();
+  });
